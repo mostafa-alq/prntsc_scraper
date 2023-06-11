4b8fdf6f6b9af7d3fc2ea7f457ba7859de708e04
mostafa-alqadi <109628361+mostafa-alqadi@users.noreply.github.com>
2023-06-11 22:24:25 +0100

    unrequired

--- sizeTest.py
+++
@@ -1,7 +0,0 @@
-import os
-fileName = 'C:\Zone\code\Webscraping\scrapedImages\IIb7oYCLRfOb5vf8955LwA.jpeg'
-fileStats = os.stat(fileName)
-
-print(fileStats)
-print(f'File size in bytes is {fileStats.st_size}')
-print(f'File size in megabytes is {fileStats.st_size / (1024 * 1024)}')
